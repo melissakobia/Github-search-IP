78481271d68daeb3c41356ff46865dd7ce8184af
melissa kobia <melissakobia1997@gmail.com>
2019-09-30 20:41:00 +0300

    Adds a function that gets users and their repositories

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+//import { User } from '../user';
+import { UserService } from '../user-service/user.service';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { RepositoryService } from '../repository-service/repository-service.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+
+  //users:User;
+
+  profile: any;
+  repos: any;
+  userName: string;
+
+
+
+  constructor(
+    private userService: UserService, private repositoryService: RepositoryService
+
+  ) {
+
+  }
+
+
+  findUser() {
+    this.userService.updateUser(this.userName);
+    this.userService.getUserInfo()
+    this.profile = this.userService.profile;
+
+    this.repositoryService.updateUser(this.userName);
+    this.repositoryService.getRepoInformation()
+    this.repos = this.repositoryService.repos;
+  }
+
+
+
+
+  ngOnInit() {
+  }
+
+}
