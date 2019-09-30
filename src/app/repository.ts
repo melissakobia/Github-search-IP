29ffc8398bb841ce7aef2676caf47a35f318a3a7
melissa kobia <melissakobia1997@gmail.com>
2019-09-30 05:21:53 +0300

    Adds a blueprint repository class

---
+++ src/app/repository.ts
@@ -0,0 +1,17 @@
+export class Repository {
+  constructor(
+    public name:string,
+    public description: string,
+    public forks: number,
+    public html_url: string,
+    public created_at : Date
+
+  ) {
+    this.name = name;
+    this.description = description;
+    this.forks = forks;
+    this.html_url = html_url;
+    this.created_at = created_at;
+
+  }
+}
