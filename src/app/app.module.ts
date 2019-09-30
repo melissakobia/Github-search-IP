2afe2a1f4c5458f280476336a4c56b7c4a3e6a9e
melissa kobia <melissakobia1997@gmail.com>
2019-09-30 20:49:39 +0300

    Updates to include a forms module and register services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormSearchComponent } from './form-search/form-search.component';
+import { HttpClient } from 'selenium-webdriver/http';
+import { UserService } from './user-service/user.service';
+import { RepositoryComponent } from './repository/repository/repository.component';
+import{ RepositoryService } from './repository-service/repository-service.service'
+import { DatePipe } from './date.pipe';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavbarComponent,
+    FormSearchComponent,
+    RepositoryComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService,RepositoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
